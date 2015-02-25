c4b20cfabc3190117d678ae518defc5c4c3dc613
Einar Helgi <einhelgi@gmail.com>
2015-02-25 15:06:52 +0000

    eitthvað komið

---
+++ fantasy_football/AddImage.java
@@ -0,0 +1,30 @@
+package fantasy_football;
+import java.awt.*;
+import javax.swing.*;
+
+public class AddImage extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Image img;
+
+	  public AddImage(String img) {
+	    this(new ImageIcon(img).getImage());
+	  }
+
+	  public AddImage(Image img) {
+	    this.img = img;
+	    Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+	    setPreferredSize(size);
+	    setMinimumSize(size);
+	    setMaximumSize(size);
+	    setSize(size);
+	    setLayout(null);
+	  }
+
+	  public void paintComponent(Graphics g) {
+	    g.drawImage(img, 0, 0, null);
+	  }
+}
---
+++ fantasy_football/Main.java
@@ -0,0 +1,66 @@
+package fantasy_football;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class Main {
+	
+	private static JFrame f;
+	private static AddImage panel3;
+	private static int count = 0;
+	
+	static public void createAndShowGUI() {
+		//AddImage panel = new AddImage(new ImageIcon("src/Images/CetpbfB.png").getImage());
+		//AddImage panel2 = new AddImage(new ImageIcon("src/Images/hair_dryer_breakfast.png").getImage());
+		panel3 = new AddImage(new ImageIcon("src/Images/jetpack_speeding.png").getImage());
+		f = new JFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton button = new JButton(" >> JavaProgrammingForums.com <<");
+        //Add action listener to button
+        button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("You clicked the button");	
+				if(count==3) count=0;
+				if(count==0) {
+					count++;
+					switchImage(new AddImage(new ImageIcon("src/Images/CetpbfB.png").getImage()));
+				}
+				if(count==1) {
+					count++;
+					switchImage(new AddImage(new ImageIcon("src/Images/CetpbfB.png").getImage()));
+				}
+				if(count==2) {
+					count++;
+					switchImage(new AddImage(new ImageIcon("src/Images/CetpbfB.png").getImage()));
+				}
+			}
+        }); 
+		f.setLayout(new GridLayout(2, 1));
+	    f.add(button);
+		f.getContentPane().add(panel3);
+	    f.pack();
+        f.setVisible(true);
+	}
+	
+	static public void switchImage(AddImage panel) {
+		f.getContentPane().remove(panel3);
+		panel3 = panel;
+		f.getContentPane().add(panel);
+	    f.pack();
+        f.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run(){
+                createAndShowGUI();
+            }
+        });
+	}
+}
+
+
