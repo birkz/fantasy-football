411ec592898dbb84513e1d8300b449ce187215f5
hannespetur <hannespetur@gmail.com>
2015-03-08 20:31:23 +0000

    removePlayerFromTeam búið til með notkun lista

--- backend/Roster.java
+++ backend/Roster.java
@@ -1,43 +1,52 @@
 package backend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import tests.*;
 
 public class Roster {
-	private PlayerInterface[] goalkeepers;
-	private PlayerInterface goalkeeperOnField;
-	private PlayerInterface[] defenders;
-	private PlayerInterface[] defendersOnField;
-	private PlayerInterface[] midfielders;
-	private PlayerInterface[] midfieldersOnField;
-	private PlayerInterface[] strikers;
-	private PlayerInterface[] strikersOnField;
+	private List<PlayerInterface> goalkeepers;
+	private List<PlayerInterface> goalkeeperOnField;
+	private List<PlayerInterface> defenders;
+	private List<PlayerInterface> defendersOnField;
+	private List<PlayerInterface> midfielders;
+	private List<PlayerInterface> midfieldersOnField;
+	private List<PlayerInterface> strikers;
+	private List<PlayerInterface> strikersOnField;
 	private PlayerInterface captain;
 	private int numberOfPlayersOnField;
 	
 	public Roster(){
 		this.numberOfPlayersOnField = 0;
-		this.goalkeepers = new PlayerMock[2];
-		this.defenders = new PlayerMock[5];
-		this.midfielders = new PlayerMock[5];
-		this.strikers = new PlayerMock[3];
+		this.goalkeepers = new ArrayList<PlayerInterface>();
+		this.defenders = new ArrayList<PlayerInterface>();
+		this.midfielders = new ArrayList<PlayerInterface>();
+		this.strikers = new ArrayList<PlayerInterface>();
+	}
+	
+	public void removePlayerFromList(PlayerInterface player,List<PlayerInterface> list){
+		for(int i = 0 ; i < list.size() ; i++){
+			if(list.get(i).equals(player)){
+				list.remove(i);
+			}
+		}
 	}
 	
 	public void removePlayerFromTeam(PlayerInterface player){
 		String posName = player.getPositionName();
 		if (posName.equals("Goalkeeper")){
-			for(int i = 0 ; i < goalkeepers.length ; i++){
-				if(goalkeepers[i] == player){
-					System.out.println("Jebb");
-					goalkeepers[i] = null;
-				}
-			}
+			removePlayerFromList(player,goalkeepers);
+			removePlayerFromList(player,goalkeeperOnField);
 		} else if (posName.equals("Defender")){
-			
+			removePlayerFromList(player,defenders);
+			removePlayerFromList(player,defendersOnField);
 		} else if (posName.equals("Midfielder")){
-			
+			removePlayerFromList(player,midfielders);
+			removePlayerFromList(player,midfieldersOnField);
 		} else if (posName.equals("Striker")){
-			
+			removePlayerFromList(player,strikers);
+			removePlayerFromList(player,strikersOnField);
 		}
 	}
-	
 }
