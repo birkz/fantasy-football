0968870727f8f3a1443b1f743bc8dc92dff5da88
Hannes Pétur Eggertsson <hannespetur@gmail.com>
2015-03-11 16:57:32 +0000

    RosterTest update 2 WIP

--- tests/RosterTest.java
+++ tests/RosterTest.java
@@ -21,9 +21,11 @@ public class RosterTest {
 		roster = new Roster();
 		PlayerMock newplayer1 = new PlayerMock("Player 1","Goalkeeper");
 		PlayerMock newplayer2 = new PlayerMock("Player 2","Goalkeeper");
+		PlayerMock newplayer3 = new PlayerMock("Player 3","Goalkeeper");
 		players = new ArrayList<PlayerMock>();
 		players.add(newplayer1);
 		players.add(newplayer2);
+		players.add(newplayer3);
 	}
 
 	@AfterClass
@@ -40,14 +42,26 @@ public class RosterTest {
 	
 	@Test
 	public void testIfOnePlayer() {
-		roster.addPlayerToTeam(players.get(0));
+		boolean add1Goalkeeper = roster.addPlayerToTeam(players.get(0));
+		assertTrue(add1Goalkeeper);
 		String[] names = roster.getNamesOfPlayersInRoster();
 		assertEquals(names,new String[]{"Player 1", "", "", ""});
 	}
 
 	@Test
 	public void testIfTwoPlayer() {
+		boolean add1Goalkeeper = roster.addPlayerToTeam(players.get(0));
+		
+		roster.addPlayerToTeam(players.get(1));
+		String[] names = roster.getNamesOfPlayersInRoster();
+		assertEquals(names,new String[]{"Player 1, Player 2", "", "", ""});
+	}
+
+	@Test
+	public void testIfThreePlayer() {
 		roster.addPlayerToTeam(players.get(0));
+		roster.addPlayerToTeam(players.get(1));
+		roster.addPlayerToTeam(players.get(2));
 		String[] names = roster.getNamesOfPlayersInRoster();
 		assertEquals(names,new String[]{"Player 1, Player 2", "", "", ""});
 	}
