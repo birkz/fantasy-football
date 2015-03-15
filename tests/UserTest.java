ca094f942fa4a006455f62ac5f73120785a20129
birkz <sigurdurbirkir@gmail.com>
2015-03-15 23:43:07 +0000

    lysing

--- tests/UserTest.java
+++ tests/UserTest.java
@@ -12,6 +12,9 @@ public class UserTest {
 	
 	private static User user;
 
+	// This test, tests the class that holds onto the users of the game.
+	// We setup the test with one new user which takes in "user1" as its name.
+	
 	@BeforeClass
 	public static void setUp() throws Exception {
 		user = new User("user1");
@@ -22,17 +25,29 @@ public class UserTest {
 		user = null;
 	}
 	
+	// This test checks if the user has a Roster
 	@Test
 	public void checkRoster() {
 		assertNotNull(user.getRoster());
 	}
 	
+	// This test checks if a name change goes though
 	@Test 
 	public void nameChange() {
 		user.changeName("user2");
 		assertEquals("user2", user.getName());
 	}
 	
+	// This test checks if the score from round 0 goes to index 0
+	@Test
+	public void scoreTest() {
+		user.addScore(0, 100);
+		assertEquals(100, user.getScore()[0]);
+	}
+	
+	// This test checks if the changes to the money of the user behave as intended
+	// where the user can't have negative money and no money is subtracted if that subtraction 
+	// would set the users money below zero
 	@Test
 	public void testChangingMoney() {
 		
