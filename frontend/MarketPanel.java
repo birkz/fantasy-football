6864038a47aa1c0823007919ba4a8e2181854c3f
hannespetur <hannespetur@gmail.com>
2015-03-25 12:15:28 +0000

    working filter

--- frontend/MarketPanel.java
+++ frontend/MarketPanel.java
@@ -23,13 +23,20 @@ public class MarketPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private final JTextField field;
 	private LeagueMock league;
-
+	private String player_choice;
+	private String team_choice;
+	private String pos_choice;
+	private JTable jtable;
 
 	/**
 	 * Create the panel.
 	 */
 	public MarketPanel() {
 		this.league = new LeagueMock();
+		this.player_choice = "";
+		this.team_choice = "Any";
+		this.pos_choice = "Any";
+		this.jtable = null;
 		
 		setLayout(new BorderLayout(0, 0)); 
 		
@@ -41,12 +48,16 @@ public class MarketPanel extends JPanel {
 			public void keyReleased(KeyEvent e) {
 				if(e.getKeyCode() == 10) {
 					System.out.println(field.getText() );
+					player_choice = field.getText();
+					refreshJTable();
 				}
 			}
 
 			@Override
 			public void keyTyped(KeyEvent e) {
-				System.out.println(field.getText() );
+				System.out.println(field.getText()+e.getKeyChar());
+				player_choice = field.getText()+e.getKeyChar();
+				refreshJTable();
 			}
 
 			@Override
@@ -56,6 +67,67 @@ public class MarketPanel extends JPanel {
 			}
 			
 		});
+		
+		refreshJTable();
+	}
+	
+	public JPanel createEntry() {
+		JPanel panel = new JPanel();
+		panel.add(new JLabel("hello what's up?"));
+		return panel;
+	}
+	
+	private JComboBox<String> addComboBox(List<String> choices, String flag) {
+		String[] choose = new String[choices.size()];
+		for(int i=0; i<choices.size(); ++i) {
+			choose[i] = choices.get(i);
+		}
+		final JComboBox<String> cb = new JComboBox<String>(choose);
+		
+		if(flag.equals("Team")){
+			cb.setSelectedItem(this.team_choice);
+		} else if(flag.equals("Pos")){
+			cb.setSelectedItem(this.pos_choice);
+        }
+		
+		cb.setVisible(true);
+		cb.addActionListener(new ActionListener() {
+            @SuppressWarnings("unchecked")
+			public void actionPerformed(ActionEvent e)
+            {
+            	JComboBox<String> cb = (JComboBox<String>) e.getSource();
+                String chosen = (String)cb.getSelectedItem();
+
+                if(flag.equals("Team") && !team_choice.equals(chosen)){
+                	System.out.println("TEAM: "+chosen);
+                	team_choice = chosen;
+                	refreshJTable();
+                	
+                } else if(flag.equals("Pos") && !pos_choice.equals(chosen)){
+                	System.out.println("POS: "+chosen);
+                	pos_choice = chosen;
+                	refreshJTable();
+                }
+                
+            }
+        }); 
+		return cb;
+	}
+	
+	@Override
+    public Dimension getPreferredSize() {
+        return Main.getInstance().returnSizeForPanel();
+    }
+	
+	/*
+	 * Draw JTable
+	 */
+	private void refreshJTable(){
+		jtable = getJTable(player_choice,team_choice,pos_choice);
+		JScrollPane scroll = new JScrollPane(jtable);
+		removeAll();
+		add(scroll);
+		add(scroll, BorderLayout.CENTER);
 		
 		JButton search = new JButton("SEARCH");
 		search.addActionListener(new ActionListener() {
@@ -67,17 +139,46 @@ public class MarketPanel extends JPanel {
 
 		List<TeamMock> teams = this.league.getTeams();
 		Iterator<TeamMock> teams_it = teams.iterator();
-		List<String> choices = new ArrayList<String>();
+		List<String> team_choices = new ArrayList<String>();
+		team_choices.add("Any");
 		
 		while(teams_it.hasNext()){
-			choices.add(teams_it.next().getName());
+			team_choices.add(teams_it.next().getName());
 		}
 
-		List<String> choices2 = Arrays.asList("Goalkeeper","Defender","Midfielder","Striker");
+		List<String> pos_choices = Arrays.asList("Any","Goalkeeper","Defender","Midfielder","Striker");
 		
+		JPanel wrapper = new JPanel();
+		wrapper.add(field);
+		wrapper.add(addComboBox(team_choices,"Team"));
+		wrapper.add(addComboBox(pos_choices,"Pos"));
+		wrapper.add(search);
+		
+		add(wrapper, BorderLayout.NORTH);
+		
+		setVisible(false);
+		setVisible(true);
+	}
+	
+	/*
+	 * get a new JTable object
+	 */
+	private JTable getJTable(String player_searched, String team_searched, String pos_searched){
 		String[] columnNames = {"Player","Team","Position","Price",""};
 		Object[][] data = getTableData();
-		JTable table = new JTable(data, columnNames);
+		JTable table = new JTable(data, columnNames){
+			
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+		    public boolean isCellEditable(int row, int column) {
+		    	//all cells false except column 4 (button)
+			    return (column==4);
+		    }
+		};
+		
+		table.setEnabled(true);
+		table.getColumn("Player").setPreferredWidth(220);
 		
 		Action buy_or_sell_action = new AbstractAction()
 		{
@@ -89,54 +190,14 @@ public class MarketPanel extends JPanel {
 		    }
 		};
 		
-		ButtonColumn buttonColumn = new ButtonColumn(table, buy_or_sell_action, 4);
-		buttonColumn.setMnemonic(KeyEvent.VK_B);
+		new ButtonColumn(table, buy_or_sell_action, 4);
 		
-		table.setEnabled(false);
-		table.getColumn("Player").setPreferredWidth(250);
-		JScrollPane scroll = new JScrollPane(table);
-		add(scroll);
-		
-		JPanel wrapper = new JPanel();
-		wrapper.add(field);
-		wrapper.add(addComboBox(choices));
-		wrapper.add(addComboBox(choices2));
-		wrapper.add(search);
-		
-		add(wrapper, BorderLayout.NORTH);
-		add(scroll, BorderLayout.CENTER);
+		return table;
 	}
 	
-	public JPanel createEntry() {
-		JPanel panel = new JPanel();
-		panel.add(new JLabel("hello what's up?"));
-		return panel;
-	}
-	
-	public JComboBox<String> addComboBox(List<String> choices) {
-		String[] choose = new String[choices.size()];
-		for(int i=0; i<choices.size(); ++i) {
-			choose[i] = choices.get(i);
-		}
-		final JComboBox<String> cb = new JComboBox<String>(choose);
-		cb.setVisible(true);
-		cb.addActionListener(new ActionListener() {
-            @SuppressWarnings("unchecked")
-			public void actionPerformed(ActionEvent e)
-            {
-            	JComboBox<String> cb = (JComboBox<String>) e.getSource();
-                String chosen = (String)cb.getSelectedItem();
-                System.out.println(chosen);
-            }
-        }); 
-		return cb;
-	}
-	
-	@Override
-    public Dimension getPreferredSize() {
-        return Main.getInstance().returnSizeForPanel();
-    }
-	
+	/*
+	 *  get the table data given some filters
+	 */
 	private Object[][] getTableData(){
 		Object[][] data = new Object[180][5];
 		
@@ -154,6 +215,21 @@ public class MarketPanel extends JPanel {
 			while(players_it.hasNext()){
 				PlayerMock player = (PlayerMock) players_it.next();
 				
+				// Filter
+				System.out.println(team.getName()+"  "+player.getName()+"  "+player_choice+"  "+player.getPositionName()+"  "+this.pos_choice);
+				if(!player.getName().toLowerCase().contains(player_choice)){
+					continue;
+				}
+				
+				if(!this.team_choice.equals("Any") && !team.getName().equals(this.team_choice)){
+					continue;
+				}
+				
+				if(!this.pos_choice.equals("Any") && !player.getPositionName().equals(this.pos_choice)){
+					continue;
+				}
+				
+				// If the values go through the filter, add them.
 				data[i][0] = player.getName();
 				data[i][1] = team.getName();
 				data[i][2] = player.getPositionName();
@@ -163,7 +239,8 @@ public class MarketPanel extends JPanel {
 			}
 			
 		}
-		return data;
+		Object[][] subdata = Arrays.copyOfRange(data, 0, i);
+		return subdata;
 		
 	}
 
