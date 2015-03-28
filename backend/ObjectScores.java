d99d82a980e76a1f2f4bc50949f6c3e76dd6e88e
birkz <sigurdurbirkir@gmail.com>
2015-03-28 18:50:19 +0000

    lagfæring á bug sem aðeins snillingur gæti forritað

--- backend/ObjectScores.java
+++ backend/ObjectScores.java
@@ -20,11 +20,10 @@ public class ObjectScores {
 	public void addScore(int score) {
 		int lastindex = this.scores.size();
 		int lastscore;
-		if(lastindex != 0) lastscore = this.scores.get(lastindex-1);
+		if(lastindex != 0) lastscore = this.totalscores.get(lastindex-1);
 		else lastscore = 0;
 		this.scores.add(score);
 		this.totalscores.add(lastscore + score);
-		
 	}
 	
 	public List<Integer> getScores() {
