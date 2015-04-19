ebed83af50539896b6fcc1042091c90de5d99625
birkz <sigurdurbirkir@gmail.com>
2015-04-19 03:23:59 +0000

    betri uppsetning á DesignedButton klasa

--- frontend/DesignedButton.java
+++ frontend/DesignedButton.java
@@ -7,43 +7,48 @@ import is.hi.f2a.backend.FontUtil;
 public class DesignedButton {
 
 	public static CustomButton orangeStyle(String text, int style, float size) {
-		CustomButton button = new CustomButton(text);
-		button.setBorder(null);
-		button.setFont(FontUtil.getFont("kalinga", style, size));
-		button.setFontColor(new Color(255, 85, 28));
-		button.setHoverBackgroundColor(new Color(255, 85, 28));
-		button.setHoverFontColor(Color.WHITE);
-		button.setPressedBackgroundColor(Color.BLACK);
-		button.setFocusPainted(false);
+
+		Color backgroundcolor = null;
+		Color fontcolor = new Color(255, 85, 28);
+		Color hoverbackcolor = new Color(255, 85, 28);
+		Color hoverfontcolor = Color.WHITE;
+		Color pressedbackcolor = Color.BLACK;
 		
-		return button;
+		return newButton(text, style, size, "kalinga", backgroundcolor, fontcolor, hoverbackcolor, hoverfontcolor, pressedbackcolor);
 	}
 	
 	public static CustomButton deleteStyle(String text, int style, float size) {
-		CustomButton button = new CustomButton(text);
-		button.setBorder(null);
-		button.setFont(FontUtil.getFont("kalinga", style, size));
-		button.setFontColor(new Color(235, 20, 20));
-		button.setHoverBackgroundColor(new Color(255, 0, 0));
-		button.setHoverFontColor(Color.WHITE);
-		button.setPressedBackgroundColor(Color.BLACK);
-		button.setFocusPainted(false);
+
+		Color backgroundcolor = null;
+		Color fontcolor = new Color(235, 20, 20);
+		Color hoverbackcolor = new Color(255, 0, 0);
+		Color hoverfontcolor = Color.WHITE;
+		Color pressedbackcolor = Color.BLACK;
 		
-		return button;
+		return newButton(text, style, size, "kalinga", backgroundcolor, fontcolor, hoverbackcolor, hoverfontcolor, pressedbackcolor);
 	}
 	
 	public static CustomButton endStyle(String text, int style, float size) {
-		CustomButton button = new CustomButton(text);
-		button.setBorder(null);
-		button.setFont(FontUtil.getFont("kalinga", style, size));
-		button.setBackground(new Color(255, 85, 28));
-		button.setFontColor(Color.WHITE);
-		button.setHoverBackgroundColor(new Color(212,32,32));
-		button.setHoverFontColor(Color.WHITE);
-		button.setPressedBackgroundColor(Color.BLACK);
-		button.setFocusPainted(false);
 		
-		return button;
+		Color backgroundcolor = new Color(255, 85, 28);
+		Color fontcolor = Color.WHITE;
+		Color hoverbackcolor = new Color(212,32,32);
+		Color hoverfontcolor = Color.WHITE;
+		Color pressedbackcolor = Color.BLACK;
+		
+		return newButton(text, style, size, "kalinga", backgroundcolor, fontcolor, hoverbackcolor, hoverfontcolor, pressedbackcolor);
+		
+	}
+	
+	public static CustomButton newStyle(String text, int style, float size) {
+		
+		Color backgroundcolor = new Color(10, 125, 10);
+		Color fontcolor = Color.WHITE;
+		Color hoverbackcolor = new Color(212,32,32);
+		Color hoverfontcolor = Color.WHITE;
+		Color pressedbackcolor = Color.BLACK;
+		
+		return newButton(text, style, size, "kalinga", backgroundcolor, fontcolor, hoverbackcolor, hoverfontcolor, pressedbackcolor);
 	}
 	
 	public static CustomButton buyStyle(int style, float size) {
@@ -60,15 +65,15 @@ public class DesignedButton {
 		return button;
 	}
 	
-	public static CustomButton newStyle(String text, int style, float size) {
+	private static CustomButton newButton(String text, int style, float size, String fontname, Color backgroundcolor, Color fontcolor, Color hoverbackcolor, Color fonthovercolor, Color pressedbackcolor) {
 		CustomButton button = new CustomButton(text);
 		button.setBorder(null);
-		button.setFont(FontUtil.getFont("kalinga", style, size));
-		button.setBackground(new Color(10, 125, 10));
-		button.setFontColor(Color.WHITE);
-		button.setHoverBackgroundColor(new Color(212,32,32));
-		button.setHoverFontColor(Color.WHITE);
-		button.setPressedBackgroundColor(Color.BLACK);
+		button.setFont(FontUtil.getFont(fontname, style, size));
+		button.setBackground(backgroundcolor);
+		button.setFontColor(fontcolor);
+		button.setHoverBackgroundColor(hoverbackcolor);
+		button.setHoverFontColor(fonthovercolor);
+		button.setPressedBackgroundColor(pressedbackcolor);
 		button.setFocusPainted(false);
 		
 		return button;
